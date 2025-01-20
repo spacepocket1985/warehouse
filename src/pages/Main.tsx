fa997759c86e07f602f8de0809180caf68ea5965
Space Pocket <space1985pocket@gmail.com>
2025-01-20 08:33:08 +0300

    fix: first loading data with token

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,36 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getToken } from '../utils/getToken';
 import { useGetItemListQuery } from '../store/slices/apiSlice';
-import { getTokenFromLS } from '../utils/localStorageActions';
-import { useAppDispatch, useAppSelector } from '../hooks/storeHooks';
 
+import { useAppDispatch, useAppSelector } from '../hooks/storeHooks';
 import { setWarehouseTotal } from '../store/slices/warehouseSlice';
 import { ItemsList } from '../components/items/itemsList/itemsList';
 import { ListFooter } from '../components/items/listFooter/ListFooter';
 import { Spinner } from '../components/spinner/Spinner';
 
 const Main: React.FC = () => {
-  useEffect(() => {
-    const fetchToken = async () => {
-      await getToken();
-    };
-    fetchToken();
-  }, []);
-
   const dispatch = useAppDispatch();
+  const [isTokenLoaded, setIsTokenLoaded] = useState<boolean>(false);
 
   const { page, pageSize, sortOrder, itemName } = useAppSelector(
     (state) => state.warehouse
   );
-  const token = getTokenFromLS();
-  const skip = !token;
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      await getToken();
+      setIsTokenLoaded(true);
+    };
+    fetchToken();
+  }, []);
+
+  const skip = !isTokenLoaded;
 
   const { data, isFetching, isError } = useGetItemListQuery(
     { page, pageSize, sortOrder, itemName },
     { skip }
   );
 
-  const errorMsg = 'Error fetching currencies. Please try refreshing';
+  const errorMsg =
+    'Ошибка при получении данных. Пожалуйста, попробуйте обновить страницу.';
 
   const contentOrSpinner = isFetching ? (
     <Spinner />
